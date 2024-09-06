721b7c76be923db15db2104a9e09cb3ac6d8c1e9
Sanagavarapu sri koustubh <119048897+koustubh-007@users.noreply.github.com>
2024-09-06 11:36:01 +0530

    Update dirserarch.py

--- dirserarch.py
+++ dirserarch.py
@@ -13,14 +13,18 @@ def run_command(command):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     return result.stdout.strip()
 
+def dirsearch(domain,flags):
+    if not command_exists("dirsearch"):
+        exit(1)
+        
+    print("[+] Running dirsearch...")
+    dirsearch_output = run_command(f"python dirsearch.py -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,sqlasp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip -l {domain}/403.txt -i 200 --full-url")
+    print(f"[+] Dirsearch results saved to {domain}/dirsearch-403.txt")
+    
+    print("[+] Running dirsearch...")
+    dirsearch_output = run_command(f"python dirsearch.py -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,sqlasp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip -l {domain}/404.txt -i 200 --full-url")
+    print(f"[+] Dirsearch results saved to {domain}/dirsearch-404.txt")
 
-if command_exists("dirsearch"):
-        print("[+] Running dirsearch...")
-        dirsearch_output = run_command(f"python dirsearch.py -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,sqlasp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip -l {domain}/403.txt -i 200 --full-url")
-        print(f"[+] Dirsearch results saved to {domain}/dirsearch-403.txt")
+    
 
-
-if command_exists("dirsearch"):
-        print("[+] Running dirsearch...")
-        dirsearch_output = run_command(f"python dirsearch.py -e php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,sqlasp,aspx,aspx~,asp~,py,py~,rb,rb~,php,php~,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,jsp~,lock,log,rar,old,sql,sql.gz,sql.zip,sql.tar.gz,sql~,swp,swp~,tar,tar.bz2,tar.gz,txt,wadl,zip -l {domain}/404.txt -i 200 --full-url")
-        print(f"[+] Dirsearch results saved to {domain}/dirsearch-404.txt")
+    
