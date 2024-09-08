e66b59bf76a783a47bafa1458395ec89c50faa1c
Sanagavarapu sri koustubh <119048897+koustubh-007@users.noreply.github.com>
2024-09-08 23:49:43 +0530

    Update dirsearch.py

--- dirsearch.py
+++ dirsearch.py
@@ -1,6 +1,5 @@
 import subprocess
 
-
 def command_exists(command):
     """Check if a command exists on the system."""
     return subprocess.call(f"type {command}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
@@ -12,11 +11,11 @@ def run_command(command):
     return result.stdout.strip()
 
 
-def dirsearch_cmd(path):
+def dirsearch_cmd(in_path, out_path):
     if not command_exists("dirsearch"):
         exit(1)
     extensions = "php,asp,aspx,jsp,py,txt,conf,config,bak,backup,swp,old,db,sql,asp,aspx,py,rb,bak,bkp,cache,cgi,conf,csv,html,inc,jar,js,json,jsp,lock,log,rar,old,sql,sql.gz,tar,tar.gz,txt,wadl,zip"
-    return f"python3 dirsearch.py -e {extensions} -l -o {path} -i 200 --full-url"
+    return f"python3 dirsearch.py -e {extensions} -l {in_path} -o {out_path} -i 200 --full-url"
 
     
 
